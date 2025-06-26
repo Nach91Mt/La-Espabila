d3001ea83f8c0c26f34d2355d3bbf5f0fb5364f1
Nacho <ignamt91@gmail.com>
2025-06-26 10:22:34 +0200

    added alergen in bdd

--- backend/app/routes/food.py
+++ backend/app/routes/food.py
@@ -1,26 +1,30 @@
 from flask import Blueprint, jsonify, request
 from app.database import db
 from app.models import User,Food, Section
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 food_bp = Blueprint("food_bp", __name__)
 
 @food_bp.route("/api/add_food", methods=["POST"])
+@jwt_required()
 def add_food():
     data = request.get_json()
     section_name = data.get("section_name")
     name = data.get("name")
     description = data.get("description")
     price = data.get("price")
+    alergens = data.get("allergens")
     image_url = data.get("image_url")
 
-    if not name or not price  or not section_name:
+    if not name or not price  or not section_name or not alergens:
         return {"error": "Faltan campos obligatorios"}, 400
+    
     section = Section.query.filter_by(name=section_name).first()
     if not section:
         section = Section(name=section_name)
         db.session.add(section)
         db.session.flush()
-    new_food = Food(name=name, description=description, price=price, image_url=image_url,section_id=section.id) 
+    new_food = Food(name=name, description=description, price=price, image_url=image_url,section_id=section.id, allergens=alergens) 
     db.session.add(new_food)
     db.session.commit()
 
